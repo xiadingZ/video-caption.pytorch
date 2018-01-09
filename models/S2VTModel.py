ba7553985fb3519b9cc214908597f376fb7387c3
xiading <976924170@qq.com>
2018-01-09 17:45:14 +0800

    fix a bug of s2vt

--- models/S2VTModel.py
+++ models/S2VTModel.py
@@ -26,19 +26,21 @@ class S2VTModel(nn.Module):
         self.use_attention = use_attention
         self.sos_id = sos_id
         self.eos_id = eos_id
-        self.embedding = nn.Embedding(self.dim_output, self.dim_hidden)
+        self.embedding = nn.Embedding(self.dim_output, self.dim_word)
 
         self.out = nn.Linear(self.dim_hidden, self.dim_output)
 
     def forward(self, vid_feats, target_variable=None,
                 teacher_forcing_ratio=1):
         batch_size, n_frames, _ = vid_feats.shape
-        padding_words = vid_feats.new(batch_size, 1, self.dim_word)
-        padding_frames = vid_feats.new(batch_size, 1, self.dim_vid)
+        padding_words = vid_feats.new(batch_size, 1, self.dim_word).zero_()
+        padding_frames = vid_feats.new(batch_size, 1, self.dim_vid).zero_()
         use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
         state1 = None
         state2 = None
         for i in range(n_frames):
+            self.rnn1.flatten_parameters()
+            self.rnn2.flatten_parameters()
             output1, state1 = self.rnn1(vid_feats[:, i, :].unsqueeze(1), state1)
             input2 = torch.cat((output1, padding_words), dim=2)
             output2, state2 = self.rnn2(input2, state2)
@@ -49,26 +51,30 @@ class S2VTModel(nn.Module):
             for i in range(self.max_length - 1):
                 # <eos> doesn't input to the network
                 current_words = self.embedding(target_variable[:, i])
+                self.rnn1.flatten_parameters()
+                self.rnn2.flatten_parameters()
                 output1, state1 = self.rnn1(padding_frames, state1)
                 input2 = torch.cat((output1, current_words.unsqueeze(1)), dim=2)
                 output2, state2 = self.rnn2(input2, state2)
                 logits = self.out(output2.squeeze(1))
                 logits = F.log_softmax(logits, dim=1)
-                seq_probs.append(logits)
+                seq_probs.append(logits.unsqueeze(1))
             seq_probs = torch.cat(seq_probs, 1)
 
         else:
             current_words = self.embedding(Variable(torch.LongTensor([self.sos_id] * batch_size)).cuda())
             for i in range(self.max_length - 1):
+                self.rnn1.flatten_parameters()
+                self.rnn2.flatten_parameters()
                 output1, state1 = self.rnn1(padding_frames, state1)
                 input2 = torch.cat((output1, current_words.unsqueeze(1)), dim=2)
                 output2, state2 = self.rnn2(input2, state2)
                 logits = self.out(output2.squeeze(1))
                 logits = F.log_softmax(logits, dim=1)
-                seq_probs.append(logits)
+                seq_probs.append(logits.unsqueeze(1))
                 _, preds = torch.max(logits, 1)
                 current_words = self.embedding(preds)
-                seq_preds.append(preds.unsqueeze)
+                seq_preds.append(preds.unsqueeze(1))
             seq_probs = torch.cat(seq_probs, 1)
             seq_preds = torch.cat(seq_preds, 1)
         return seq_probs, seq_preds
